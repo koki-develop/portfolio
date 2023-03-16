501ecee3c38aa10f4d2db3e376c7b3b89077c964
koki-develop <kou.pg.0131@gmail.com>
2023-03-17 08:43:34 +0900

    mantine 導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
+import { MantineProvider } from "@mantine/core";
 import type { AppProps } from "next/app";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: "light",
+      }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 };
 
 export default App;
