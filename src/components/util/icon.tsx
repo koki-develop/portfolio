bbec6016972134044e3540b46486266fe80d00b6
koki-develop <kou.pg.0131@gmail.com>
2023-03-17 22:03:26 +0900

    icon を追加

--- src/components/util/icon.tsx
+++ src/components/util/icon.tsx
@@ -1,14 +1,20 @@
-import GitHubIcon from "@/../public/images/icons/github.svg";
-import GoIcon from "@/../public/images/icons/go.svg";
-import TwitterIcon from "@/../public/images/icons/twitter.svg";
-import ZennIcon from "@/../public/images/icons/zenn.svg";
+import GoIcon from "@/../public/icons/skill/go.svg";
+import JavaScriptIcon from "@/../public/icons/skill/javascript.svg";
+import TypeScriptIcon from "@/../public/icons/skill/typescript.svg";
+import GitHubIcon from "@/../public/icons/social/github.svg";
+import TwitterIcon from "@/../public/icons/social/twitter.svg";
+import ZennIcon from "@/../public/icons/social/zenn.svg";
 import React from "react";
 
 const Icons = {
+  // social
   github: GitHubIcon,
   twitter: TwitterIcon,
   zenn: ZennIcon,
+  // language
   go: GoIcon,
+  javascript: JavaScriptIcon,
+  typescript: TypeScriptIcon,
 } as const;
 
 export type IconName = keyof typeof Icons;
