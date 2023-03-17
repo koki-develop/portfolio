46dbed8c57dd231aebc0a6d335d03f967bd92e0f
koki-develop <kou.pg.0131@gmail.com>
2023-03-17 22:49:09 +0900

    skill を追加

--- config/skill.ts
+++ config/skill.ts
@@ -1,12 +1,25 @@
-import { getSkill, SkillGroup } from "@/components/model/skill";
-
-const go = getSkill("go");
-const javascript = getSkill("javascript");
-const typescript = getSkill("typescript");
+import { SkillGroup } from "@/components/model/skill";
+import * as skills from "@/components/model/skill";
 
 export const skillGroups: SkillGroup[] = [
   {
     name: "Language",
-    skills: [go, javascript, typescript],
+    skills: [skills.go, skills.javascript, skills.typescript],
+  },
+  {
+    name: "Framework/Library",
+    skills: [skills.gin, skills.express, skills.react, skills.nextjs],
+  },
+  {
+    name: "Cloud",
+    skills: [skills.aws, skills.googleCloud],
+  },
+  {
+    name: "CI/CD",
+    skills: [skills.githubActions, skills.circleci],
+  },
+  {
+    name: "Other",
+    skills: [skills.terraform, skills.docker],
   },
 ];
