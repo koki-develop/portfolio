ab32b9e2c76587600f517b98d7a06b0f0767146c
koki-develop <kou.pg.0131@gmail.com>
2023-03-17 09:28:42 +0900

    Icon コンポーネントを作成

---
+++ src/components/util/icon.tsx
@@ -0,0 +1,22 @@
+import React, { memo } from "react";
+import GitHubIcon from "@/../public/images/icons/github.svg";
+import TwitterIcon from "@/../public/images/icons/twitter.svg";
+import ZennIcon from "@/../public/images/icons/zenn.svg";
+
+const icons = {
+  github: GitHubIcon,
+  twitter: TwitterIcon,
+  zenn: ZennIcon,
+} as const;
+
+export type IconProps = {
+  icon: keyof typeof icons;
+} & React.SVGProps<SVGElement>;
+
+const Icon: React.FC<IconProps> = (props) => {
+  const { icon, ...svgProps } = props;
+
+  return React.createElement(icons[icon], svgProps);
+};
+
+export default Icon;
