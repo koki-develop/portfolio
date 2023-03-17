bb6ad7c5ed8130659a16480973f975acc06ab92a
koki-develop <kou.pg.0131@gmail.com>
2023-03-17 12:35:35 +0900

    config を作成

---
+++ config/user.ts
@@ -0,0 +1,17 @@
+import { User } from "@/components/model/user";
+import dayjs from "dayjs";
+
+const birth = dayjs("1998-01-31");
+const age = dayjs().diff(birth, "years");
+
+export const user: User = {
+  name: "Koki Sato",
+  tag: "Developer",
+  description: `埼玉県在住の ${age} 歳。\nのんびり生きています。`,
+  email: "kou.pg.0131@gmail.com",
+  socials: {
+    github: { url: "https://github.com/koki-develop" },
+    twitter: { url: "https://twitter.com/koki_develop" },
+    zenn: { url: "https://zenn.dev/kou_pg_0131" },
+  },
+};
