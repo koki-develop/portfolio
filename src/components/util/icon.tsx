82c485ce708173da56258b6a9f96346b3c5d397e
koki-develop <kou.pg.0131@gmail.com>
2023-03-17 12:53:48 +0900

    一部修正

--- src/components/util/icon.tsx
+++ src/components/util/icon.tsx
@@ -1,22 +1,26 @@
 import GitHubIcon from "@/../public/images/icons/github.svg";
+import GoIcon from "@/../public/images/icons/go.svg";
 import TwitterIcon from "@/../public/images/icons/twitter.svg";
 import ZennIcon from "@/../public/images/icons/zenn.svg";
-import React, { memo } from "react";
+import React from "react";
 
-const icons = {
+const Icons = {
   github: GitHubIcon,
   twitter: TwitterIcon,
   zenn: ZennIcon,
+  go: GoIcon,
 } as const;
 
+export type IconName = keyof typeof Icons;
+
 export type IconProps = {
-  icon: keyof typeof icons;
+  icon: IconName;
 } & React.SVGProps<SVGElement>;
 
 const Icon: React.FC<IconProps> = (props) => {
   const { icon, ...svgProps } = props;
 
-  return React.createElement(icons[icon], svgProps);
+  return React.createElement(Icons[icon], svgProps);
 };
 
 export default Icon;
