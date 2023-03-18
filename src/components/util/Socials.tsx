41461ee4fb237d0dae74add1f4b84d2dd1f37e9f
koki-develop <kou.pg.0131@gmail.com>
2023-03-18 21:24:36 +0900

    Socials をリファクタ

--- src/components/util/Socials.tsx
+++ src/components/util/Socials.tsx
@@ -1,15 +1,29 @@
-import Icon from "./Icon";
+import Icon, { IconName } from "@/components/util/Icon";
 import Link from "@/components/util/Link";
 import { Socials as SocialsModel } from "@/model/social";
 import { Box } from "@mantine/core";
-import React, { memo } from "react";
+import React, { memo, useMemo } from "react";
 
 export type SocialsProps = {
   socials: SocialsModel;
 };
 
+type Item = {
+  url: string;
+  icon: IconName;
+};
+
 const Socials: React.FC<SocialsProps> = memo((props) => {
   const { socials } = props;
+
+  const items = useMemo((): Item[] => {
+    return [
+      { url: socials.github.url, icon: "github" },
+      { url: socials.twitter.url, icon: "twitter" },
+      { url: socials.zenn.url, icon: "zenn" },
+    ];
+  }, [socials.github.url, socials.twitter.url, socials.zenn.url]);
+
   return (
     <Box
       sx={(theme) => ({
@@ -17,18 +31,11 @@ const Socials: React.FC<SocialsProps> = memo((props) => {
         gap: theme.spacing.lg,
       })}
     >
-      {/* github */}
-      <Link href={socials.github.url} external>
-        <Icon icon="github" width={40} height={40} />
-      </Link>
-      {/* twitter */}
-      <Link href={socials.twitter.url} external>
-        <Icon icon="twitter" width={40} height={40} />
-      </Link>
-      {/* zenn */}
-      <Link href={socials.zenn.url} external>
-        <Icon icon="zenn" width={40} height={40} />
-      </Link>
+      {items.map((item) => (
+        <Link key={item.icon} href={socials.github.url} external>
+          <Icon icon={item.icon} width={40} height={40} />
+        </Link>
+      ))}
     </Box>
   );
 });
