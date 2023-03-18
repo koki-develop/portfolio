8df13f7a1b6df92d0e1a881141cdef8835346030
koki-develop <kou.pg.0131@gmail.com>
2023-03-18 09:53:05 +0900

    experiences を設定

---
+++ config/experience.ts
@@ -0,0 +1,23 @@
+import { Experience } from "@/components/model/experience";
+
+export const experiences: Experience[] = [
+  {
+    title: "株式会社スリーシェイクに入社",
+    from: "2022-06",
+  },
+  {
+    title: "株式会社レッドジャーニーにて業務委託として従事",
+    from: "2021-08",
+    to: "2022-05",
+  },
+  {
+    title: "株式会社オプトに入社",
+    from: "2019-07",
+    to: "2022-05",
+  },
+  {
+    title: "未経験で SES 企業に入社",
+    from: "2018-06",
+    to: "2019-05",
+  },
+];
--- config/index.ts
+++ config/index.ts
@@ -1,4 +1,5 @@
 import { certifications } from "./certification";
+import { experiences } from "./experience";
 import { skillGroups } from "./skill";
 import { user } from "./user";
 
@@ -6,4 +7,5 @@ export const config = {
   user,
   skillGroups,
   certifications,
+  experiences,
 };
