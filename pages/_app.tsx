7cd71ccf2807138a49ad4c361d4060fedebad137
koki-develop <kou.pg.0131@gmail.com>
2023-03-18 11:51:41 +0900

    Google Analytics を導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,26 @@ import Layout from "@/components/Layout";
 import "@fontsource/open-sans";
 import { AnchorProps, MantineProvider } from "@mantine/core";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import Script from "next/script";
+import { useEffect } from "react";
+
+const handleRouteChangeComplete = (pathname: string) => {
+  window.gtag("config", process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID, {
+    page_path: pathname,
+  });
+};
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on("routeChangeComplete", handleRouteChangeComplete);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChangeComplete);
+    };
+  }, [router.events]);
+
   return (
     <MantineProvider
       withGlobalStyles
@@ -32,6 +50,23 @@ const App = ({ Component, pageProps }: AppProps) => {
       }}
     >
       <Layout>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+        `,
+          }}
+        />
         <Component {...pageProps} />
       </Layout>
     </MantineProvider>
