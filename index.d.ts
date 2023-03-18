2674ec136e9892c13e926f04e1d274c8e4140c2a
koki-develop <kou.pg.0131@gmail.com>
2023-03-18 11:41:59 +0900

    NEXT_PUBLIC_GA_MEASUREMENT_ID 環境変数の型定義を追加

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,9 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly NEXT_PUBLIC_GA_MEASUREMENT_ID: string;
+  }
+}
+
 declare module "*.svg" {
   const content: React.FC<React.SVGProps<SVGElement>>;
   export default content;
