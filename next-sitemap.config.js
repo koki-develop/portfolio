a74b5f282ec42e7179dfad715b7ce712f047f7d9
koki-develop <kou.pg.0131@gmail.com>
2023-03-18 20:50:53 +0900

    next-sitemap の設定を更新

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,19 @@
 module.exports = {
   siteUrl: "https://koki.me",
   generateRobotsTxt: true,
-}
+  transform: async (config, path) => {
+    /** @type import('next-sitemap').ISitemapField */
+    const field = {
+      loc: path,
+      lastmod: new Date().toISOString(),
+      priority: config.priority,
+    };
+
+    switch (path) {
+      case "/":
+        return { ...field, priority: 1.0, changefreq: "daily" };
+      default:
+        return { ...field, changefreq: "yearly" };
+    }
+  },
+};
