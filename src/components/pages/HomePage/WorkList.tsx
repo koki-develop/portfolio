501b94e24691bf3e1d1e0876d8650b0f149ee5a4
koki-develop <kou.pg.0131@gmail.com>
2023-03-20 07:01:06 +0900

    skills を表示

--- src/components/pages/HomePage/WorkList.tsx
+++ src/components/pages/HomePage/WorkList.tsx
@@ -1,7 +1,8 @@
+import Icon from "@/components/util/Icon";
 import Link from "@/components/util/Link";
 import Paper from "@/components/util/Paper";
 import { repositoryUrl, Work } from "@/model/work";
-import { Box, Grid, Stack, Text, Title } from "@mantine/core";
+import { Box, Grid, Group, Stack, Text, Title, Tooltip } from "@mantine/core";
 import React, { memo } from "react";
 
 export type WorkListProps = {
@@ -17,12 +18,33 @@ const WorkList: React.FC<WorkListProps> = memo((props) => {
         <Grid.Col key={work.name} span={12} sm={6}>
           <Paper p="md" sx={{ height: "100%" }}>
             <Stack spacing="sm" sx={{ height: "100%" }}>
-              <Box sx={{ display: "flex" }}>
-                <Link external href={work.url ?? repositoryUrl(work)}>
-                  <Title order={4} size="h3">
-                    {work.name}
-                  </Title>
-                </Link>
+              <Box>
+                <Box sx={{ display: "flex", marginBottom: 4 }}>
+                  <Link external href={work.url ?? repositoryUrl(work)}>
+                    <Title order={4} size="h3">
+                      {work.name}
+                    </Title>
+                  </Link>
+                </Box>
+
+                <Box sx={{ display: "flex" }}>
+                  {work.skills.map((skill, index) => (
+                    <Tooltip key={skill.name} label={skill.name} withArrow>
+                      <Paper
+                        sx={{
+                          zIndex: work.skills.length - index,
+                          borderRadius: "50%",
+                          height: 28,
+                          width: 28,
+                          overflow: "hidden",
+                          marginLeft: index !== 0 ? -8 : undefined,
+                        }}
+                      >
+                        <Icon icon={skill.icon} height="100%" width="100%" />
+                      </Paper>
+                    </Tooltip>
+                  ))}
+                </Box>
               </Box>
 
               <Text sx={{ flexGrow: 1 }}>{work.description}</Text>
