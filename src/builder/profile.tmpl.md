5920da90fa60c38f327d54bf16633a358dd17bbb
koki-develop <kou.pg.0131@gmail.com>
2023-03-20 15:06:58 +0900

    profile.ts を実装

---
+++ src/builder/profile.tmpl.md
@@ -0,0 +1,42 @@
+[![Zenn Likes](https://badgen.org/img/zenn/kou_pg_0131/likes?style=plastic)](https://zenn.dev/kou_pg_0131)
+[![Qiita Contributions](https://badgen.org/img/qiita/koki_develop/contributions?style=plastic)](https://qiita.com/koki_develop)
+
+# Overview
+
+<p>
+<%= config.user.description %>
+</p>
+
+## Socials
+
+<% Object.values(config.socials).forEach(function (social) { -%>
+[<img src="./public/icons/<%= social.iconPath %>" width="40" height="40" />](<%= social.url %>)
+<% }) -%>
+
+## Skills
+
+<% config.skillGroups.forEach(function (skillGroup) { -%>
+<% skillGroup.skills.forEach(function (skill) { -%>
+[<img src="./public/icons/<%= skill.iconPath %>" width="40" height="40" />](<%= skill.url %>)
+<% }) -%>
+<% }) -%>
+
+## Works
+
+<% config.workGroups.forEach(function (workGroup) { -%>
+### <%= workGroup.name %>
+
+| Name | Repository |
+| --- | --- |
+<% workGroup.works.forEach(function (work) { -%>
+| <% if (work.url) { -%><a href="<%= work.url %>"><%= work.name -%></a><% } else { -%><%= work.name -%><% } -%> | <a href="https://github.com/koki-develop/<%= work.repository %>">koki-develop/<%= work.repository -%></a> |
+<% }) -%>
+<% }) -%>
+
+## Contact
+
+<% Object.values(config.socials).forEach(function (social) { -%>
+[<img src="./public/icons/<%= social.iconPath %>" width="40" height="40" />](<%= social.url %>)
+<% }) -%>
+
+[<%= config.user.email %>](mailto:<%= config.user.email %>)
---
+++ src/builder/profile.ts
@@ -0,0 +1,64 @@
+import { config } from "@/../config";
+import { IconName } from "@/components/util/Icon";
+import ejs from "ejs";
+import fs from "fs";
+import path from "path";
+
+const iconPath = (icon: IconName): string =>
+  ((
+    {
+      // skill
+      aws: "skill/aws.svg",
+      bubbletea: "skill/bubbletea.svg",
+      circleci: "skill/circleci.svg",
+      cobra: "skill/cobra.svg",
+      cypress: "skill/cypress.svg",
+      dagger: "skill/dagger.svg",
+      docker: "skill/docker.svg",
+      express: "skill/express.svg",
+      gin: "skill/gin.svg",
+      githubactions: "skill/githubactions.svg",
+      go: "skill/go.svg",
+      googlecloud: "skill/googlecloud.svg",
+      javascript: "skill/javascript.svg",
+      mantine: "skill/mantine.svg",
+      materialui: "skill/materialui.svg",
+      nextjs: "skill/nextjs.svg",
+      react: "skill/react.svg",
+      serverless: "skill/serverless.svg",
+      tailwindcss: "skill/tailwindcss.svg",
+      terraform: "skill/terraform.svg",
+      typescript: "skill/typescript.svg",
+      vecty: "skill/vecty.svg",
+      vercel: "skill/vercel.svg",
+      // social
+      github: "social/github.svg",
+      twitter: "social/twitter.svg",
+      zenn: "social/zenn.svg",
+    } as Record<IconName, string>
+  )[icon]);
+
+(async () => {
+  const template = fs
+    .readFileSync(path.join(__dirname, "profile.tmpl.md"))
+    .toString();
+
+  const readme = ejs.render(template, {
+    config: {
+      ...config,
+      socials: {
+        github: { ...config.socials.github, iconPath: iconPath("github") },
+        twitter: { ...config.socials.twitter, iconPath: iconPath("twitter") },
+        zenn: { ...config.socials.zenn, iconPath: iconPath("zenn") },
+      },
+      skillGroups: config.skillGroups.map((skillGroup) => ({
+        ...skillGroup,
+        skills: skillGroup.skills.map((skill) => ({
+          ...skill,
+          iconPath: iconPath(skill.icon),
+        })),
+      })),
+    },
+  });
+  fs.writeFileSync("profile.md", readme);
+})();
