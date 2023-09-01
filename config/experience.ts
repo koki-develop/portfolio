3cf3a817605f664169fb1b77ce7454a6ddc92485
koki-develop <kou.pg.0131@gmail.com>
2023-09-01 09:21:57 +0900

    経歴を更新

--- config/experience.ts
+++ config/experience.ts
@@ -2,8 +2,17 @@ import { Experience } from "@/model/experience";
 
 export const experiences: Experience[] = [
   {
+    title: "Classi株式会社に入社",
+    from: "2023-09",
+  },
+  {
+    title: "株式会社レッドジャーニーにて業務委託として従事",
+    from: "2023-06",
+  },
+  {
     title: "株式会社スリーシェイクに入社",
     from: "2022-06",
+    to: "2023-08",
   },
   {
     title: "株式会社レッドジャーニーにて業務委託として従事",
