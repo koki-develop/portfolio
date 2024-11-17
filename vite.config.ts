e4cbbd0e095e76951c1f29b214a73e33c86dd042
koki-develop <kou.pg.0131@gmail.com>
2024-11-17 23:14:05 +0900

    Add svgr plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from "@vitejs/plugin-react";
 import vike from "vike/plugin";
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 import vercel from "vite-plugin-vercel";
 
 export default defineConfig({
@@ -9,6 +10,7 @@ export default defineConfig({
       prerender: true,
     }),
     react({}),
+    svgr(),
     vercel(),
   ],
 });
