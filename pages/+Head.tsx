37d8bead30c213ed62ef5028e4e246dd6d6e7305
koki-develop <kou.pg.0131@gmail.com>
2024-11-21 07:44:41 +0900

    Set favicon.ico

--- pages/+Head.tsx
+++ pages/+Head.tsx
@@ -1,9 +1,9 @@
-import logoUrl from "../assets/logo.svg";
+import faviconUrl from "../assets/favicon.ico";
 
 export default function HeadDefault() {
   return (
     <>
-      <link rel="icon" href={logoUrl} />
+      <link rel="icon" href={faviconUrl} />
     </>
   );
 }
