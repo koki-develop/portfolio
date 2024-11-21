961d5b0074075885c50ce12fa0213ae6f91420bc
koki-develop <kou.pg.0131@gmail.com>
2024-11-21 21:11:08 +0900

    Remove unused className

--- pages/index/Card.tsx
+++ pages/index/Card.tsx
@@ -2,10 +2,9 @@ import type { FC, ReactNode } from "react";
 
 export type CardProps = {
   children: ReactNode;
-  className?: string;
 };
 
-const Card: FC<CardProps> = ({ children, className }) => {
+const Card: FC<CardProps> = ({ children }) => {
   return (
     <div className="bg-gray-800 rounded-lg p-6 border border-gray-700">
       {children}
