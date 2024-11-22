8bed32f3535a9f9682996ef7e567cdc6b12056bc
koki-develop <kou.pg.0131@gmail.com>
2024-11-22 23:46:04 +0900

    Add className prop

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,11 +1,17 @@
 import type { FC, ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
 export type ContainerProps = {
   children: ReactNode;
+  className?: string;
 };
 
-const Container: FC<ContainerProps> = ({ children }) => {
-  return <div className="mx-auto max-w-4xl max-lg:px-4">{children}</div>;
+const Container: FC<ContainerProps> = ({ children, className }) => {
+  return (
+    <div className={twMerge("mx-auto max-w-4xl max-lg:px-4", className)}>
+      {children}
+    </div>
+  );
 };
 
 export default Container;
