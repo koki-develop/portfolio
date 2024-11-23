9f5dc8a8577dc438c016dc9382eaf6f12a1a9ed6
koki-develop <kou.pg.0131@gmail.com>
2024-11-23 22:41:29 +0900

    Add className prop to Card component

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,12 +1,19 @@
 import type { FC, ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
 export type CardProps = {
   children: ReactNode;
+  className?: string;
 };
 
-const Card: FC<CardProps> = ({ children }) => {
+const Card: FC<CardProps> = ({ children, className }) => {
   return (
-    <div className="rounded-lg border border-gray-700 bg-gray-800 p-6">
+    <div
+      className={twMerge(
+        "rounded-lg border border-gray-700 bg-gray-800 p-4",
+        className,
+      )}
+    >
       {children}
     </div>
   );
