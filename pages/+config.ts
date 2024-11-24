7a4be648d75c66ef42d88aa90c22b10e21f09b9a
koki-develop <kou.pg.0131@gmail.com>
2024-11-24 10:42:43 +0900

    Remove comments

--- pages/+config.ts
+++ pages/+config.ts
@@ -2,14 +2,9 @@ import vikeReact from "vike-react/config";
 import type { Config } from "vike/types";
 import Layout from "../layout/Layout";
 
-// Default config (can be overridden by pages)
-// https://vike.dev/config
-
 export default {
-  // https://vike.dev/Layout
   Layout,
 
-  // https://vike.dev/head-tags
   title: "Koki Sato",
   description: "のんびり生きています。",
 
