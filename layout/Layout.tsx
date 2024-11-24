59ce67e5d61a363bd4a1e0f40eeef91e6868effe
koki-develop <kou.pg.0131@gmail.com>
2024-11-24 12:08:28 +0900

    Prevent web font flicker

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,21 +1,25 @@
 import "@fontsource/inter";
 import "./style.css";
 import "./tailwind.css";
+import clsx from "clsx";
 import type { FC, ReactNode } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import { useFontLoaded } from "./hooks";
 
 export type LayoutProps = {
   children: ReactNode;
 };
 
 const Layout: FC<LayoutProps> = ({ children }) => {
+  const show = useFontLoaded("Inter");
+
   return (
-    <>
+    <div className={clsx({ hidden: !show })}>
       <Header />
       <main className="pt-4 pb-16">{children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
 
